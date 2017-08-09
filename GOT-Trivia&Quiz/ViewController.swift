a0e2dfd31cbdd5c0f7046437594ca98694bbb7c3
kieran-foley <kieran-19@hotmail.co.uk>
2017-08-09 12:39:34 +0100

    first_Commit

---
+++ GOT-Trivia&Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GOT-Trivia&Quiz
+//
+//  Created by Kieran Foley on 09/08/2017.
+//  Copyright © 2017 Kieran Foley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
